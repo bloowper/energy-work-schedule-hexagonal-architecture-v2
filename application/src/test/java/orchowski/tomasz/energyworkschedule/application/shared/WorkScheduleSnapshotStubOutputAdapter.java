a86d343121a86fd78ce98426acdcfb01bf391a23
Tomasz Orchowski <tomasz.orchowski@codibly.com>
2023-02-04 17:08:41 +0100

    imp stub for work schedule snapshot output adapter bdd tests

--- application/src/test/java/orchowski/tomasz/energyworkschedule/application/shared/WorkScheduleSnapshotStubOutputAdapter.java
+++ application/src/test/java/orchowski/tomasz/energyworkschedule/application/shared/WorkScheduleSnapshotStubOutputAdapter.java
@@ -1,4 +1,24 @@
 package orchowski.tomasz.energyworkschedule.application.shared;
 
-class WorkScheduleSnapshotStubOutputAdapter {
+import orchowski.tomasz.energyworkschedule.application.port.output.WorkScheduleSnapshotOutputPort;
+import orchowski.tomasz.energyworkschedule.domain.value.Id;
+import orchowski.tomasz.energyworkschedule.domain.value.WorkSchedule;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+
+class WorkScheduleSnapshotStubOutputAdapter implements WorkScheduleSnapshotOutputPort {
+    private final Map<Id, WorkSchedule> workScheduleMap = new ConcurrentHashMap<>();
+
+    @Override
+    public Optional<WorkSchedule> getWorkScheduleSnapshot(Id deviceId) {
+        return Optional.ofNullable(workScheduleMap.get(deviceId));
+    }
+
+    @Override
+    public WorkSchedule persistWorkScheduleSnapshot(WorkSchedule workSchedule, Id deviceId) {
+        workScheduleMap.put(deviceId, workSchedule);
+        return workSchedule;
+    }
 }
