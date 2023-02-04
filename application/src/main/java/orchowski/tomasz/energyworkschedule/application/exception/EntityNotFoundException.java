6aef4e9c54281fb8d77bd65689720d04c23aba19
Tomasz Orchowski <tomasz.orchowski@codibly.com>
2023-02-04 17:50:52 +0100

    change application exception

--- application/src/main/java/orchowski/tomasz/energyworkschedule/application/exception/EntityNotFoundException.java
+++ application/src/main/java/orchowski/tomasz/energyworkschedule/application/exception/EntityNotFoundException.java
@@ -3,7 +3,7 @@ package orchowski.tomasz.energyworkschedule.application.exception;
 import orchowski.tomasz.energyworkschedule.domain.value.Id;
 
 public class EntityNotFoundException extends RuntimeException {
-    public EntityNotFoundException(Class<?> entityType, Id id) {
-        super("Not found [%s] with id: [%s]".formatted(entityType.getName(), id));
+    public EntityNotFoundException(Class<?> type, Id id) {
+        super("Not found [%s] with id: [%s]".formatted(type.getName(), id));
     }
 }
