d70075048954c15fc343fe83a5e446b44ef52d03
Tomasz Orchowski <tomasz.orchowski@codibly.com>
2023-02-05 20:34:10 +0100

    adding new bdd tests for application hexagon

--- application/src/test/java/orchowski/tomasz/energyworkschedule/application/port/input/DeviceManagementInputPortTest.groovy
+++ application/src/test/java/orchowski/tomasz/energyworkschedule/application/port/input/DeviceManagementInputPortTest.groovy
@@ -5,7 +5,7 @@ import orchowski.tomasz.energyworkschedule.domain.value.Id
 
 class DeviceManagementInputPortTest extends BaseSpecification {
 
-    def "Add new device"() {
+    def "User should be able to add new device"() {
         given: //Valid device
         def id = Id.generateNew()
         def device = deviceManagementUseCase.createDevice(id)
@@ -18,4 +18,28 @@ class DeviceManagementInputPortTest extends BaseSpecification {
         fetchDevice.get() != null
     }
 
+    def "User should be able to remove device"() {
+        given: // Device is added to system previously
+        def id = Id.generateNew()
+        def device = deviceManagementUseCase.createDevice(id)
+        deviceManagementUseCase.persistDevice(device)
+
+        when: // User remove device by id
+        deviceManagementUseCase.removeDevice(id)
+
+        then: // System should not contain device
+        deviceManagementUseCase.fetchDevice(id).isEmpty()
+    }
+
+    def "User should be able to find device by id"() {
+        given: // Device is added to system previously
+        def id = Id.generateNew()
+        deviceManagementUseCase.persistDevice(deviceManagementUseCase.createDevice(id))
+
+        when: // User search device by id
+        def device = deviceManagementUseCase.fetchDevice(id)
+
+        then: // Device is returned to the user
+        device.isPresent()
+    }
 }
---
+++ application/src/test/java/orchowski/tomasz/energyworkschedule/application/port/input/PolicyManagementInputPortTest.groovy
@@ -0,0 +1,54 @@
+package orchowski.tomasz.energyworkschedule.application.port.input
+
+import orchowski.tomasz.energyworkschedule.application.shared.BaseSpecification
+import orchowski.tomasz.energyworkschedule.domain.value.Id
+import orchowski.tomasz.energyworkschedule.domain.value.Priority
+import orchowski.tomasz.energyworkschedule.domain.value.TimePeriod
+
+import java.time.Duration
+import java.time.Instant
+
+class PolicyManagementInputPortTest extends BaseSpecification {
+
+    def "User should be able to add new policy to device"() {
+        given: "User previously added device"
+        def deviceId = Id.generateNew()
+        deviceManagementUseCase.persistDevice(deviceManagementUseCase.createDevice(deviceId))
+
+        and: "User has specified time period for new policy"
+        def timePeriod = new TimePeriod(Instant.now(), Instant.now().plus(Duration.ofHours(10)))
+
+        and: "User has specified priority for new policy"
+        def priority = new Priority(100)
+
+
+        and: "User has specified max allowed power usage for policy"
+        def maxPowerUsage = 1500
+
+        when: "User start process of adding new policy with given specified values"
+        // User create new policy
+        def policy = policyManagementUseCase.createPowerUsagePolicy(
+                timePeriod,
+                priority,
+                maxPowerUsage
+        )
+        policyManagementUseCase.addPolicyToDevice(policyManagementUseCase.fetchDevice(deviceId).get(), policy)
+
+        then: "System should contain new policy for device"
+        policyManagementUseCase.fetchDevice(deviceId).isPresent()
+        policyManagementUseCase.fetchDevice(deviceId).get().getPolicies()
+                .stream()
+                .filter { (it.getEffectiveDate() == timePeriod) }
+                .filter { (it.getPriority() == priority) }
+                .filter { (it.getMaxPowerUsageRule().getValue() == maxPowerUsage) }
+                .findAny().isPresent()
+    }
+
+    def "User should be able to remove policy from device"() {
+        //todo
+    }
+
+    def "User should be able to reschedule policy"() {
+        //todo
+    }
+}
