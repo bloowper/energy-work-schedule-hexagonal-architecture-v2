595fde6d45d7fb2b0279497f9e13c822965e732c
Tomasz Orchowski <tomasz.orchowski@codibly.com>
2023-02-17 00:49:39 +0100

    input adapter PUT -> GET for getting work shcedule

--- framework/src/main/java/orchowski/tomasz/energyworkschedule/framework/adapter/input/rest/WorkScheduleViewInputAdapter.java
+++ framework/src/main/java/orchowski/tomasz/energyworkschedule/framework/adapter/input/rest/WorkScheduleViewInputAdapter.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import orchowski.tomasz.energyworkschedule.application.usecase.WorkScheduleViewUseCase;
 import orchowski.tomasz.energyworkschedule.domain.value.Id;
 import orchowski.tomasz.energyworkschedule.domain.value.WorkSchedule;
+import org.mapstruct.MappingTarget;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 class WorkScheduleViewInputAdapter {
     private final WorkScheduleViewUseCase workScheduleViewUseCase;
 
-    @PutMapping("/{device-uuid}/work-schedule")
+    @GetMapping("/{device-uuid}/work-schedule")
     WorkSchedule fetchWorkSchedule(@PathVariable("device-uuid") String deviceUuid) {
         return workScheduleViewUseCase.getWorkScheduleForDevice(Id.withId(deviceUuid));
     }
